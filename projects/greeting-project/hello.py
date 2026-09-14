637d3167fbfa8926c398a7d0301d66e915bfcbc9
xinranchennnnnnnn <112905252+xinranchennnnnnnn@users.noreply.github.com>
2026-09-13 20:49:12 -0700

    Create greeting project

---
+++ projects/greeting-project/hello.py
@@ -0,0 +1,8 @@
+# My first Python project in GitHub
+
+def greet_user():
+    name = input("Enter your name: ")
+    print(f"Hello, {name}! Welcome to Data Science.")
+
+if __name__ == "__main__":
+    greet_user()
